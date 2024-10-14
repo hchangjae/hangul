089acf7ee4a4983954b99798e347c271cf7715c3
hchangjae <hcj@payw.org>
2024-10-14 10:21:28 +0900

    feat: 배포

--- src/components/registry/NextAuthRegistry.tsx
+++ src/components/registry/NextAuthRegistry.tsx
@@ -2,12 +2,19 @@ import { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 import { ReactNode } from 'react';
 
+const isNextAuthOn = process.env.NEXTAUTH_URL && process.env.NEXTAUTH_SECRET
+
 type Props = {
   session: Session;
   children: ReactNode;
 };
+
 const NextAuthRegistry = (props: Props) => {
   const { session, children } = props;
+  
+  if (!isNextAuthOn) {
+    return <>{children}</>;
+  }
 
   return <SessionProvider session={session}>{children}</SessionProvider>;
 };
